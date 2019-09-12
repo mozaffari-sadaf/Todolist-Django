ccf200abe1e8f881deae4cef703896bdd5e04b40
mozaffari-sadaf <mozaffari.sadaf@gmail.com>
2019-09-12 10:17:32 +0430

    Add django-heroku

--- todolist/settings.py
+++ todolist/settings.py
@@ -140,3 +140,8 @@ import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+import django_heroku
+# Activate Django-Heroku.
+django_heroku.settings(locals())
