f4346922aef622b5acea1c3a301f11057ca554ed
mozaffari-sadaf <mozaffari.sadaf@gmail.com>
2019-09-13 10:40:00 +0430

    Change urlpatterns for easier access

--- todolist/urls.py
+++ todolist/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-	path('todoapp/', include('todoapp.urls')),
+	path('', include('todoapp.urls')),
     path('admin/', admin.site.urls),
 ]
