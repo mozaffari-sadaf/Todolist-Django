d45d76c13987901e3817cb3c220ef9e2d43b7860
mozaffari-sadaf <mozaffari.sadaf@gmail.com>
2019-09-13 11:32:48 +0430

    Change the LOGIN_REDIRECT_URL

--- todolist/settings.py
+++ todolist/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 
-LOGIN_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = '/'
 
 LOGIN_URL = '/login'
 
